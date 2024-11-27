f043cd6e33f25509c40b0264aa1aaf0110c32569
Michael Bond <mbond@fusionww.com>
2024-11-27 16:58:59 -0500

    Test MAUI markup with sample page

---
+++ MauiApp1/Pages/SampleContentPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Maui.Markup;
+
+namespace MauiApp1.Pages;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class SampleContentPage : ContentPage
+{
+    public SampleContentPage() =>
+        Content = new Grid
+        {
+            RowDefinitions = GridRowsColumns.Rows.Define([(Row.TextEntry, 36)]),
+            ColumnDefinitions = GridRowsColumns.Columns.Define(
+            [
+                (Column.Description, GridRowsColumns.Star),
+                (Column.Input, GridRowsColumns.Stars(2))
+            ]),
+            Children =
+            {
+                new Label()
+                    .Text("Code:")
+                    .Row(Row.TextEntry).Column(Column.Description),
+                new Entry { Keyboard = Keyboard.Numeric, }.Row(Row.TextEntry).Column(Column.Input)
+                    .BackgroundColor(Colors.AliceBlue)
+                    .FontSize(15)
+                    .Placeholder("Enter number")
+                    .TextColor(Colors.Black)
+                    .Height(44)
+                    .Margin(5, 5)
+                    .Bind(Entry.TextProperty,
+                        static vm => vm.RegistrationCode,
+                        static (ViewModel vm, string text) => vm.RegistrationCode = text)
+            }
+        };
+
+    private enum Row { TextEntry }
+
+    private enum Column { Description, Input }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class ViewModel
+{
+    public string RegistrationCode { get; set; } = string.Empty;
+}
